3ee5d5a17c0984cb9cb0bc7e0136fea1b9bb5f69
chen <a675750333@gmail.com>
2024-02-23 15:34:16 +0800

    feat: 新增springdoc

---
+++ member/src/main/java/xyz/chen/member/config/SpringdocConfig.java
@@ -0,0 +1,33 @@
+package xyz.chen.member.config;
+
+
+import io.swagger.v3.oas.models.Components;
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.security.SecurityRequirement;
+import io.swagger.v3.oas.models.security.SecurityScheme;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SpringdocConfig {
+    String schemeName = "bearerAuth";
+    String bearerFormat = "JWT";
+    String scheme = "bearer";
+
+    @Bean
+    public OpenAPI caseOpenAPI() {
+        return new OpenAPI()
+                .addSecurityItem(new SecurityRequirement()
+                        .addList(schemeName)).components(new Components()
+                        .addSecuritySchemes(
+                                schemeName, new SecurityScheme()
+                                        .name(schemeName)
+                                        .type(SecurityScheme.Type.HTTP)
+                                        .bearerFormat(bearerFormat)
+                                        .in(SecurityScheme.In.HEADER)
+                                        .scheme(scheme)
+                        )
+                );
+    }
+}
+
