e24c7a9aa591dac49d92aed04348ddc73ac04573
chen <a675750333@gmail.com>
2024-02-28 15:33:07 +0800

    feat: 启用方法权限控制

--- library-management-app/src/main/java/xyz/chen/commons/base/ExceptionController.java
+++ library-management-app/src/main/java/xyz/chen/commons/base/ExceptionController.java
@@ -1,6 +1,7 @@
 package xyz.chen.commons.base;
 
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.access.AccessDeniedException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 import org.springframework.web.servlet.NoHandlerFoundException;
@@ -20,6 +21,11 @@ public class ExceptionController {
     public BaseResponse<String> noHandlerException(Exception e) {
 //        log.warn("触发异常: {},异常描述: {}", e.getClass().getName(), e.getMessage());
         return BaseResponse.fail(STATUS_CODE.UNKNOWN_ERROR, e.getLocalizedMessage());
+    }
+
+    @ExceptionHandler(AccessDeniedException.class)
+    public BaseResponse<String> accessDeniedException(Exception e) {
+        return BaseResponse.fail(STATUS_CODE.ACCESS_DENIED_ERROR);
     }
 
 }
