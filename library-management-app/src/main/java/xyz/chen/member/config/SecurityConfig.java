7c7baae848ec3afd445d04c31926fae9d096583d
chen <a675750333@gmail.com>
2024-03-01 14:34:55 +0800

    refactor: 调整安全配置，关闭状态管理

--- library-management-app/src/main/java/xyz/chen/member/config/SecurityConfig.java
+++ library-management-app/src/main/java/xyz/chen/member/config/SecurityConfig.java
@@ -11,6 +11,7 @@ import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configurers.AbstractHttpConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -54,19 +55,21 @@ public class SecurityConfig {
 
     @Bean
     public SecurityFilterChain filterChain(HttpSecurity http) throws Exception {
-        http.csrf(AbstractHttpConfigurer::disable);
-        http.authorizeHttpRequests(authorize -> authorize
+        http
+                .csrf(AbstractHttpConfigurer::disable)
+                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .authorizeHttpRequests(authorize -> authorize
                 .requestMatchers(HttpMethod.OPTIONS, "/**").permitAll()
                 .requestMatchers("/actuator", "/actuator/**").permitAll()
                 .requestMatchers("/swagger-ui/**", "/swagger-ui.html", "/api-docs/**").permitAll()
                 .requestMatchers(configData.getPermitAllUrls().toArray(String[]::new)).permitAll()
                 .anyRequest().authenticated()
-        );
-        http.oauth2Login(Customizer.withDefaults());
-        http.exceptionHandling(exception -> exception.accessDeniedHandler(accessDeniedHandler())
-                .authenticationEntryPoint(authenticationEntryPoint()));
-        http.addFilterAt(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class);
-        http.addFilterAt(filterExceptionHandler(), SecurityContextHolderFilter.class);
+                )
+                .oauth2Login(Customizer.withDefaults())
+                .exceptionHandling(exception -> exception.accessDeniedHandler(accessDeniedHandler())
+                        .authenticationEntryPoint(authenticationEntryPoint()))
+                .addFilterAt(jwtAuthenticationFilter(), UsernamePasswordAuthenticationFilter.class)
+                .addFilterAt(filterExceptionHandler(), SecurityContextHolderFilter.class);
 
         return http.build();
     }
