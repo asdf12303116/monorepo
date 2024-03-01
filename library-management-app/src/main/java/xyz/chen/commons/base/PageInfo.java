4b9d9705d16657f1e22fe4c2527a6c64d7a8fb9d
chen <a675750333@gmail.com>
2024-03-01 15:51:02 +0800

    feat: 启用分页查询插件

--- library-management-app/src/main/java/xyz/chen/commons/base/PageInfo.java
+++ library-management-app/src/main/java/xyz/chen/commons/base/PageInfo.java
@@ -1,11 +1,21 @@
 package xyz.chen.commons.base;
 
+import com.baomidou.mybatisplus.extension.plugins.pagination.Page;
+import lombok.AllArgsConstructor;
 import lombok.Data;
 
 @Data
+@AllArgsConstructor
 public class PageInfo {
-    private Integer pageNum;
-    private Integer pageSize;
-    private Integer total;
+    private Long pageNum;
+    private Long pageSize;
+    private Long total;
 
+    public PageInfo(Page<?> page) {
+        this.pageNum = page.getPages();
+        this.pageSize = page.getSize();
+        this.total = page.getTotal();
+    }
 }
+
+
