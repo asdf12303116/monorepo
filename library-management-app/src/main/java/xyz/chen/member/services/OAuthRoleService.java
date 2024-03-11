11db404da691a1c8e9bb51a768801f3d96407018
chen <a675750333@gmail.com>
2024-03-11 10:51:52 +0800

    fix: 修复oauth登录时,若用户不在映射组时无法成功注册

--- library-management-app/src/main/java/xyz/chen/member/services/OAuthRoleService.java
+++ library-management-app/src/main/java/xyz/chen/member/services/OAuthRoleService.java
@@ -3,6 +3,7 @@ package xyz.chen.member.services;
 import com.baomidou.mybatisplus.extension.service.impl.ServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
+import xyz.chen.commons.config.ConfigData;
 import xyz.chen.member.entity.OAuthRoleMap;
 import xyz.chen.member.entity.Role;
 import xyz.chen.member.repository.OAuthRoleRepository;
@@ -15,11 +16,17 @@ public class OAuthRoleService extends ServiceImpl<OAuthRoleRepository, OAuthRole
 
     @Autowired
     private RoleRepository roleRepository;
+    @Autowired
+    private ConfigData configData;
 
     public List<Role> getRolesByGroups(List<String> groups) {
-        List<Long> roleIds = this.lambdaQuery().in(OAuthRoleMap::getGroupUUID, groups)
-                .select(OAuthRoleMap::getRoleId).list().stream().map(OAuthRoleMap::getRoleId).toList();
-
+        List<Long> roleIds = this.lambdaQuery()
+                .select(OAuthRoleMap::getRoleId)
+                .in(OAuthRoleMap::getGroupUUID, groups)
+                .list().stream().map(OAuthRoleMap::getRoleId).toList();
+        if (roleIds.isEmpty()) {
+            return roleRepository.selectBatchIds(List.of(configData.getDefaultOauthRole()));
+        }
 
         return roleRepository.selectBatchIds(roleIds);
 
