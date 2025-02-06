351b7d7a0869dda2f69517a2a2bee849ef4c7fbb
Chen <a675750333@gmail.com>
2025-02-06 22:10:13 +0800

    refactor(repo): 调整项目为monorepo

---
+++ backend/lma/src/test/java/xyz/chen/member/services/BookBorrowServiceTest.java
@@ -0,0 +1,20 @@
+package xyz.chen.member.services;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class BookBorrowServiceTest {
+
+    @Test
+    void searchBookBorrow() {
+    }
+
+    @Test
+    void saveBookBorrow() {
+    }
+
+    @Test
+    void deleteBookBorrow() {
+    }
+}
